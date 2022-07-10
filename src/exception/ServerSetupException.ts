90aac62cc8022f522e41997b4ee2cbf19cdf89b5
sehej singh <sehedge.singh@gmail.com>
2022-07-10 13:12:45 +0530

    added custom exceptions

---
+++ src/exception/ServerSetupException.ts
@@ -0,0 +1,1 @@
+export default class ServerSetupException extends Error {}
---
+++ src/exception/index.ts
@@ -0,0 +1,11 @@
+import InvalidParameterException from './InvalidParameterException';
+import BadRequestException from './BadRequestException';
+import UnauthoriseException from './UnauthoriseException';
+import ServerSetupException from './ServerSetupException';
+
+export default {
+  InvalidParameterException,
+  BadRequestException,
+  UnauthoriseException,
+  ServerSetupException,
+};
