fcb938b20dc034abd1466ce28b0211bb09b3f915
sehej singh <sehedge.singh@gmail.com>
2022-07-13 18:40:00 +0530

    only extracting restaurantName from restaurantInfo

--- src/db/service/RestaurantService.ts
+++ src/db/service/RestaurantService.ts
@@ -79,6 +79,7 @@ export default class RestaurantService {
         {
           model: RestaurantModel,
           as: 'restaurantInfo',
+          attributes: ['restaurantName'],
         },
       ],
     });
